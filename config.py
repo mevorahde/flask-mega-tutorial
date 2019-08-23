c5826c0a60a6c56ee2f6739759395372cc681b36
David Mevorah <mevorahde@hotmail.com>
2019-08-22 20:40:57 -0700

    Update microblog to include ch14

--- config.py
+++ config.py
@@ -13,3 +13,5 @@ class Config(object):
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
     ADMINS = ['dmevorah.python.programs@gmail.com']
     POSTS_PER_PAGE = 25
+    LANGUAGES = ['en', 'es']
+    MS_TRANSLATOR_KEY = os.environ.get('MS_TRANSLATOR_KEY')
